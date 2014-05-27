89e042292f95cb67f9b7f7e9eb2af4ab46e02201
eavalenzuela7 <eavalenzuela@usfca.edu>
2014-05-27 11:51:11 -0700

    adding additional project files

---
+++ autoprepVisual/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  autoprepVisual
+//
+//  Created by Valenzuela, Eric A. on 5/23/14.
+//  Copyright (c) 2014 University of San Francisco. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
